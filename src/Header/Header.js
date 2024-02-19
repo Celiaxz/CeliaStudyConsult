b788b85300e198a606a7ae89bdf936a99b6a9376
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2024-02-19 22:23:17 +0100

    complete nav

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,16 @@
 import React from "react";
-let Links = [
-  { name: "HOME", link: "/" },
-  { name: "SERVICE", link: "/" },
-  { name: "ABOUT", link: "/" },
-  { name: "BOOKING", link: "/" },
-  { name: "CONTACT", link: "/" },
-];
+import Button from "./Button";
+import { useState } from "react";
 
 function Header() {
+  let Links = [
+    { name: "HOME", link: "/" },
+    { name: "SERVICE", link: "/" },
+    { name: "ABOUT", link: "/" },
+    { name: "BOOKING", link: "/" },
+    { name: "CONTACT", link: "/" },
+  ];
+  let [open, setOpen] = useState(false);
   return (
     <div className="shadow-md w-full fixed top-0 left-0">
       <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
@@ -17,9 +20,19 @@ function Header() {
           </span>{" "}
           Celia Study Consult{" "}
         </div>
-        <ul className="md:flex md:items-center">
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden "
+        >
+          <ion-icon name={open ? "close" : "menu"}></ion-icon>
+        </div>
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in  ${
+            open ? "top-20 " : "top-[-490px] md:opacity-100 opacity-0"
+          }`}
+        >
           {Links.map((link) => (
-            <li key={link.name} className="md:ml-8 text-xl">
+            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
               <a
                 href={link.link}
                 className="text-gray-800 hover:text-orange-400 duration-500"
@@ -28,6 +41,7 @@ function Header() {
               </a>
             </li>
           ))}
+          <Button>Get Started</Button>
         </ul>
       </div>
     </div>
